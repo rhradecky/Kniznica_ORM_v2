cc9ef551ec75bd732b345ac789d22e6ce74b2ea0
PY-TU-10-23 <rhradecky@gmail.com>
2024-04-30 21:22:00 +0200

    opravit chubu

---
+++ book.py
@@ -0,0 +1,15 @@
+from main import db
+
+class Book(db):
+    __tablename__ = 'books'
+
+    book_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30))
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.author_id'), nullable=False)
+    genre_id = db.Column(db.Integer, db.ForeignKey('genres.genre_id'))
+    isbn = db.Column(db.String(13))
+    publication_year = db.Column(db.Integer)
+    copies = db.Column(db.Integer, default=1)
+
+    author = db.relationship("Author", back_populates="books")
+    genre = db.relationship("Genre", back_populates="books")
